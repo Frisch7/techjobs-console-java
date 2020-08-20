65a33b8c719543c7e7b4afea19452115cb43dac4
Dominic Frisch <frisch7@gmail.com>
2020-08-19 21:14:22 -0500

    Finished Assignment

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -75,10 +76,36 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
+            aValue = aValue.toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
+        }
+
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value){
+        loadData();
+        String jobValue = "";
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs) {
+
+            for (Map.Entry<String, String> item: job.entrySet()) {
+
+                jobValue = item.getValue();
+                jobValue = jobValue.toLowerCase();
+                value = value.toLowerCase();
+                if(jobValue.contains(value)){
+                    if(!jobs.contains(job)) {
+                        jobs.add(job);
+                    }
+                }
+            }
+
+
         }
 
         return jobs;
